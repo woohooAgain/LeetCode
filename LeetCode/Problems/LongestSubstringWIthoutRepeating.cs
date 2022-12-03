930e70e4c35d239ea3664af31653c0e7e926d02f
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-03 17:07:19 +0300

    Longest substring wthout repeating

---
+++ LeetCode/Problems/LongestSubstringWIthoutRepeating.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class LongestSubstringWithoutRepeating
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            var queue = new Queue<char>(s.Length);
+            var max = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (queue.Contains(c))
+                {
+                    max = max > queue.Count ? max : queue.Count;
+                    while (queue.Contains(c))
+                    {
+                        queue.Dequeue();
+                    }
+                }
+                queue.Enqueue(c);
+            }
+            return max > queue.Count ? max : queue.Count;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/LongestSubstringWIthoutRepeatingTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class LongestSubstringWithoutRepeatingTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            var result = LongestSubstringWithoutRepeating.LengthOfLongestSubstring("abcabcbb");
+            Assert.AreEqual(3, result);
+
+            result = LongestSubstringWithoutRepeating.LengthOfLongestSubstring("bbbbb");
+            Assert.AreEqual(1, result);
+
+            result = LongestSubstringWithoutRepeating.LengthOfLongestSubstring("pwwkew");
+            Assert.AreEqual(3, result);
+
+            result = LongestSubstringWithoutRepeating.LengthOfLongestSubstring("dvdf");
+            Assert.AreEqual(3, result);
+        }
+    }
+}
