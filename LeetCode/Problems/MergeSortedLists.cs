50e1ba0781dc259d4711332c08f58e6ab055e8c6
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-03 21:50:29 +0300

    Merge sorted lists

---
+++ LeetCode/Problems/MergeSortedLists.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class MergeSortedLists
+    {
+        public static ListNode MergeKLists(ListNode[] lists)
+        {
+            var currentNodeArray = new ListNode[lists.Length];
+            for(var i = 0; i < lists.Length; i++)
+            {
+                currentNodeArray[i] = lists[i];
+            }
+            var minValueNode = GetNextNode(currentNodeArray);
+            var result = minValueNode;
+            var currentNode = result;
+            while (minValueNode != null)
+            {
+                minValueNode = GetNextNode(currentNodeArray);
+                currentNode.next = minValueNode;
+                currentNode = currentNode.next;
+            }
+            return result;
+        }
+
+        private static ListNode GetNextNode(ListNode[] currentNodeArray)
+        {
+            var length = currentNodeArray.Length;
+            int selectedIndex = 0;
+            var minValueNode = default(ListNode);
+            for (var i = 0; i < length; i++)
+            {
+                if (currentNodeArray[i] != null)
+                {
+                    selectedIndex = i;
+                    minValueNode = currentNodeArray[i];
+                    break;
+                }
+            }
+            if (minValueNode == null)
+            {
+                return null;
+            }
+            //var selectedIndex = 0;
+            for(var i = 1; i < length; i++)
+            {
+                if (currentNodeArray[i] != null && currentNodeArray[i].val < minValueNode.val)
+                {
+                    selectedIndex = i;
+                    minValueNode = currentNodeArray[i];
+                }
+            }
+            currentNodeArray[selectedIndex] = currentNodeArray[selectedIndex].next;
+            return new ListNode(minValueNode.val);
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/MergeSortedListsTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Models;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class MergeSortedListsTests
+    {
+        [TestMethod()]
+        public void MergeKListsTest()
+        {
+            var input = new ListNode[3];
+            input[0] = new ListNode(1,
+                                    new ListNode(4,
+                                                 new ListNode(5)));
+            input[1] = new ListNode(1,
+                                    new ListNode(3,
+                                                 new ListNode(4)));
+            input[2] = new ListNode(2,
+                                    new ListNode(6));
+            var result = MergeSortedLists.MergeKLists(input);
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(1, result.next.val);
+            Assert.AreEqual(2, result.next.next.val);
+            Assert.AreEqual(3, result.next.next.next.val);
+            Assert.AreEqual(4, result.next.next.next.next.val);
+            Assert.AreEqual(4, result.next.next.next.next.next.val);
+            Assert.AreEqual(5, result.next.next.next.next.next.next.val);
+            Assert.AreEqual(6, result.next.next.next.next.next.next.next.val);
+
+            input = new ListNode[0];
+            result = MergeSortedLists.MergeKLists(input);
+            Assert.AreEqual(null, result);
+
+            input = new ListNode[1];;
+            result = MergeSortedLists.MergeKLists(input);
+            Assert.AreEqual(null, result);
+        }
+    }
+}
