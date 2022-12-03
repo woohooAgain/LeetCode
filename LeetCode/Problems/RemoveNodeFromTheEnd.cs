bf3790164a546ff5b0aec02f09d739f81f0ccbe2
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-03 17:32:21 +0300

    Remove node from the end of node list

---
+++ LeetCode/Problems/RemoveNodeFromTheEnd.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class RemoveNodeFromTheEnd
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var dict = new Dictionary<int, ListNode>();
+            var current = head;
+            var counter = 0;
+            while(current != null)
+            {
+                dict.Add(counter++, current);
+                current = current.next;
+            }
+            var index = counter - n;
+            if (index != 0)
+            {
+                if (n == 1)
+                {
+                    dict[index - 1].next = null;
+                }
+                else
+                {
+                    dict[index - 1].next = dict[index + 1];
+                }
+                return dict[0];
+            }
+            else
+            {
+                return head.next;
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/RemoveNodeFromTheEndTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class RemoveNodeFromTheEndTests
+    {
+        [TestMethod()]
+        public void RemoveNthFromEndTest()
+        {
+            var input = new Models.ListNode(1,
+                                            new Models.ListNode(2,
+                                                                new Models.ListNode(3,
+                                                                                    new Models.ListNode(4,
+                                                                                                        new Models.ListNode(5)))));
+            var result = RemoveNodeFromTheEnd.RemoveNthFromEnd(input, 2);
+            Assert.AreEqual(5, result.next.next.next.val);
+
+            input = new Models.ListNode(1);
+            result = RemoveNodeFromTheEnd.RemoveNthFromEnd(input, 1);
+            Assert.AreEqual(null, result);
+
+            input = new Models.ListNode(1,
+                                        new Models.ListNode(2));
+            result = RemoveNodeFromTheEnd.RemoveNthFromEnd(input, 1);
+            Assert.AreEqual(1, result.val);
+        }
+    }
+}
