9435fcc5ea8cef9e1e07ddd18d2458904d51d78b
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-03 16:42:53 +0300

    Height balanced tree

---
+++ LeetCode/Problems/BalancesBinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode.Problems
+{
+    public class BalancesBinaryTree
+    {
+        public static bool IsBalanced(TreeNode root)
+        {
+            int result = IsBalancedAddition(root);
+            if (result >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static int IsBalancedAddition(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            var lh = IsBalancedAddition(root.left);
+            if (lh == -1)
+            {
+                return -1;
+            }
+            var rh = IsBalancedAddition(root.right);
+            if (rh == -1)
+            {
+                return -1;
+            }
+            if (lh > rh + 1 || rh > lh + 1)
+            {
+                return -1;
+            }
+            return Math.Max(lh, rh) + 1;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/BalancesBinaryTreeTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class BalancesBinaryTreeTests
+    {
+        [TestMethod()]
+        public void IsBalancedTest()
+        {
+            var result = BalancesBinaryTree.IsBalanced(
+                new Models.TreeNode(3,
+                                    new Models.TreeNode(9),
+                                    new Models.TreeNode(20,
+                                                        new Models.TreeNode(15),
+                                                        new Models.TreeNode(7))));
+            Assert.IsTrue(result);
+
+            result = BalancesBinaryTree.IsBalanced(
+                new Models.TreeNode(1,
+                                    new Models.TreeNode(2,
+                                                        new Models.TreeNode(3,
+                                                                            new Models.TreeNode(4),
+                                                                            new Models.TreeNode(4)),
+                                                        new Models.TreeNode(3)),
+                                    new Models.TreeNode(2)));
+            Assert.IsFalse(result);
+
+            result = BalancesBinaryTree.IsBalanced(new Models.TreeNode(1,
+                                                                       null,
+                                                                       new Models.TreeNode(2,
+                                                                                           null,
+                                                                                           new Models.TreeNode(3))));
+            Assert.IsFalse(result);
+
+            result = BalancesBinaryTree.IsBalanced(new Models.TreeNode(1,
+                                                                       new Models.TreeNode(2)));
+            Assert.IsTrue(result);
+
+            result = BalancesBinaryTree.IsBalanced(new Models.TreeNode());
+            Assert.IsTrue(result);
+        }
+    }
+}
