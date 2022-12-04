e8b69da153e8754ce3a47183a115ae9207ab9d08
Ilya Kulikov <Ilya.Kulikov@comindware.ru>
2022-12-04 09:08:17 +0300

    Next permutation

---
+++ LeetCode/Problems/NextPermutation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class NextPermutation
+    {
+        public static void NextPermutationAction(int[] nums)
+        {
+            var i = nums.Length - 2;
+            while (i >= 0 && nums[i + 1] <= nums[i])
+            {
+                i--;
+            }
+            if (i >= 0)
+            {
+                var j = nums.Length - 1;
+                while (nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+            Reverse(nums, i + 1);
+        }
+
+        private static void Reverse(int[] nums, int start)
+        {
+            var i = start;
+            var j = nums.Length - 1;
+            while(i < j)
+            {
+                Swap(nums, i, j);
+                i++;
+                j--;
+            }    
+        }
+
+        private static void Swap(int[] nums, int i, int j)
+        {
+            var temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/NextPermutationTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class NextPermutationTests
+    {
+        [TestMethod()]
+        public void NextPermutationActionTest()
+        {
+            var input = new int[] { 1, 2, 3 };
+            NextPermutation.NextPermutationAction(input);
+            Assert.AreEqual(1, input[0]);
+            Assert.AreEqual(3, input[1]);
+            Assert.AreEqual(2, input[2]);
+
+            input = new int[] { 3, 2, 1 };
+            NextPermutation.NextPermutationAction(input);
+            Assert.AreEqual(1, input[0]);
+            Assert.AreEqual(2, input[1]);
+            Assert.AreEqual(3, input[2]);
+
+            input = new int[] { 1, 1, 5 };
+            NextPermutation.NextPermutationAction(input);
+            Assert.AreEqual(1, input[0]);
+            Assert.AreEqual(5, input[1]);
+            Assert.AreEqual(1, input[2]);
+
+            input = new int[] { 1, 5, 4, 1 };
+            NextPermutation.NextPermutationAction(input);
+            Assert.AreEqual(4, input[0]);
+            Assert.AreEqual(1, input[1]);
+            Assert.AreEqual(1, input[2]);
+            Assert.AreEqual(5, input[3]);
+        }
+    }
+}
